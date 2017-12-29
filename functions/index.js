2b0318926f53b3d257e2fb1b5c8e450b4aaa880d
Zarko Dencic <denciczarko@gmail.com>
2017-12-30 00:04:07 +0100

    push by server

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,14 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
+var webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
 var serviceAccount = require("./pwagram-fb-key.json");
+var privateVapidKey = require("./pwagram-vapid-key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -23,7 +25,31 @@ exports.storePostData = functions.https.onRequest(function(request, response) {
             image: request.body.image
         })
             .then(function() {
-                response.status(201).json({message: 'Data stored', id: request.body.id});
+                webpush.setVapidDetails(
+                    'mailto:denciczarko@gmail.com',
+                    'BLve4x4CextokbUexlU-RIPo8Bvb9SuhsnCKoaRkiIfqZmybkYybX0Lwn95NVDzVaMiijufGBmm6qI_21GDOMM4',
+                    privateVapidKey.key
+                );
+                return admin.database().ref('subscriptions').once('value');
+            })
+            .then(function (value) {
+                value.forEach(function (sub) {
+                    var pushConfig = {
+                        endpoint: sub.val().endpoint,
+                        keys: {
+                            auth: sub.val().keys.auth,
+                            p256dh: sub.val().keys.p256dh
+                        }
+                    };
+                    webpush.sendNotification(pushConfig, JSON.stringify({
+                        title: "New Post",
+                        content: "Added"
+                    }))
+                        .catch(function (err) {
+                            console.log(err)
+                        })
+                });
+                response.status(201).json({message: 'Data stored', id: request.body.id})
             })
             .catch(function(err) {
                 response.status(500).json({error: err});
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -49,21 +49,64 @@ function displayConfirmNotification() {
     }
 }
 
-function askForNotificationPermission() {
-  Notification.requestPermission(function (permission) {
-    console.log('User Choice', permission);
-    if(permission !== 'granted') {
-        console.log('No notification permisson granted!');
-    } else {
-      // Hide Button
-        displayConfirmNotification()
+function configurePushSub() {
+    if (!('serviceWorker' in navigator)) {
+        return;
     }
-  });
+
+    var reg;
+    navigator.serviceWorker.ready
+        .then(function(swreg) {
+            reg = swreg;
+            return swreg.pushManager.getSubscription();
+        })
+        .then(function(sub) {
+            if (sub === null) {
+                var vapidPublicKey = 'BLve4x4CextokbUexlU-RIPo8Bvb9SuhsnCKoaRkiIfqZmybkYybX0Lwn95NVDzVaMiijufGBmm6qI_21GDOMM4';
+                var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+                return reg.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidPublicKey
+                });
+            } else {
+                // We have a subscription
+            }
+        })
+        .then(function(newSub) {
+            return fetch('https://pwagram-3ce94.firebaseio.com/subscriptions.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newSub)
+            })
+        })
+        .then(function(res) {
+            if (res.ok) {
+                displayConfirmNotification();
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
 
-if ('Notification' in window) {
-    enableNotificationsButtons.forEach(function(notificationsButton) {
-        notificationsButton.style.display = 'inline-block';
-        notificationsButton.addEventListener('click', askForNotificationPermission)
-    })
+function askForNotificationPermission() {
+    Notification.requestPermission(function(result) {
+        console.log('User Choice', result);
+        if (result !== 'granted') {
+            console.log('No notification permission granted!');
+        } else {
+            configurePushSub();
+            // displayConfirmNotification();
+        }
+    });
+}
+
+if ('Notification' in window && 'serviceWorker' in navigator) {
+    for (var i = 0; i < enableNotificationsButtons.length; i++) {
+        enableNotificationsButtons[i].style.display = 'inline-block';
+        enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+    }
 }
